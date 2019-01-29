a6724189e4ad66854ebd4251d1140793733e1f08
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2019-01-29 10:28:50 +0100

    Generation de paires différentes

---
+++ src/dut/archilog/intro/CompleteCharGenerator.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class CompleteCharGenerator implements CharGenerator {
+
+    @Override
+    public List<Character> generate(int nbPairs) {
+        if (nbPairs > 26) {
+            throw new IllegalArgumentException("Ne fonctionne pas au delà de 26 pairs");
+        }
+        List<Character> cards = new ArrayList<>();
+        char current;
+        for (int i = 0; i < nbPairs; i++) {
+            current = (char) (i + 65);
+            cards.add(current);
+            cards.add(current);
+        }
+        Collections.shuffle(cards);
+        return cards;
+    }
+
+}
